8dd694bacfa5cda5362bd8067ae72de7e38dc262
Laura <laura.colazzo@mail.polimi.it>
2022-03-27 11:35:59 +0200

    Added a Model package and a PieceColor Enumeration within this new package

---
+++ src/main/java/it/polimi/ingsw/am19/Model/PieceColor.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.am19.Model;
+
+/**
+ * Enumeration for all possible pieces (students and professors) colors
+ */
+public enum PieceColor {
+    GREEN,
+    RED,
+    YELLOW,
+    PINK,
+    BLUE
+}
