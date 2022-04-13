55f79800f5c480225f3db4d1a8d103680f58d882
Laura <laura.colazzo@mail.polimi.it>
2022-04-13 16:11:47 +0200

    ExtraInfluenceCard skeleton added

---
+++ src/main/java/it/polimi/ingsw/am19/Model/CharacterCards/ExtraInfluenceCard.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.am19.Model.CharacterCards;
+
+import it.polimi.ingsw.am19.Model.BoardManagement.Island;
+import it.polimi.ingsw.am19.Model.Match.Match;
+import it.polimi.ingsw.am19.Model.Utilities.PieceColor;
+
+public class ExtraInfluenceCard extends CharacterCard{
+    public ExtraInfluenceCard(Match match) {
+
+    }
+
+    @Override
+    public void activateEffect(PieceColor color, Island island) {
+
+    }
+
+    @Override
+    public void initialAction() {
+
+    }
+}
